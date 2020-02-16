9fbd5ae0bf43a70fa3447a5c5ed7efe30c2017e6
Jackson <jack@forlogic.org>
2020-02-15 23:13:38 -0300

    Provai e vede ajustado

--- ColetaneaDeLouvor/Forms/FormProvaieVede.cs
+++ ColetaneaDeLouvor/Forms/FormProvaieVede.cs
@@ -1,17 +1,6 @@
-﻿using ColetaneaDeLouvor.DAO;
-using ColetaneaDeLouvor.Model;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using System;
 using System.Diagnostics;
-using System.Drawing;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ColetaneaDeLouvor.Forms
@@ -28,85 +17,76 @@ namespace ColetaneaDeLouvor.Forms
             dtVideo.Value = DateTime.Now;//datetime recebe a data atual
         }
 
+        String getMes(Int32 mes)
+        {
+            switch (mes) {
+                case 1:
+                    return "01_janeiro";
+                case 2:
+                    return "02_fevereiro";
+                case 3:
+                    return "03_março";
+                case 4:
+                    return "04_abril";
+                case 5:
+                    return "05_maio";
+                case 6:
+                    return "06_junho";
+                case 7:
+                    return "07_julho";
+                case 8:
+                    return "08_agosto";
+                case 9:
+                    return "09_setembro";
+                case 10:
+                    return "10_outubro";
+                case 11:
+                    return "11_novembro";
+                default:
+                case 12:
+                    return "12_dezembro";
+            }
+    }
+
         private void btnAbrir_Click(object sender, EventArgs e)
         {
-            DateTime escolhido = dtVideo.Value;
+            abrir();    
+        }
 
-            //Se o dia atual for domingo, quarta ou sábado
-            if ((int)escolhido.DayOfWeek == 0 || (int)escolhido.DayOfWeek == 3 || (int)escolhido.DayOfWeek == 6)
-            {
-                CultureInfo culture = new CultureInfo("pt-BR");
-                DateTimeFormatInfo dtfi = culture.DateTimeFormat;
+        void abrir()
+        {
+            var data = dtVideo.Value;
+            var diaSemana = "erro";
 
-                string dia = Convert.ToString(escolhido.Day);
-                string ano = Convert.ToString(escolhido.Year);
-                string mesNum = Convert.ToString(escolhido.Month);
-                string mesExtenso = culture.TextInfo.ToTitleCase(dtfi.GetMonthName(escolhido.Month));
-                string diaSemana = culture.TextInfo.ToTitleCase(dtfi.GetDayName(escolhido.DayOfWeek));
-
-                if (mesNum.Length == 1) //se tiver apenas um caracter, um 0 a esquerda será add no mês
-                    mesNum = mesNum.PadLeft(2, '0');
-
-                if (dia.Length == 1) //se tiver apenas um caracter, um 0 a esquerda será add no dia
-                    dia = dia.PadLeft(2, '0');
-
-                mesExtenso = (Char.ToLower(mesExtenso[0]) + mesExtenso.Substring(1)); //tornar a primeira letra em minúscula
-
-                string caminhoArquivo = string.Empty;
-
-                try
-                {
-                    DirectoryInfo dir = new DirectoryInfo(@"arquivos\\provai_e_vede\\" + diaSemana + "\\" + mesNum + "_" + mesExtenso + "\\");
-                    List<string> lista = new List<string>();
-
-                    switch(diaSemana)
-                    {
-                        case "Sábado":
-                            lista = ProvaieVede.Sabado(mesExtenso);
-                            break;
-                        case "Domingo":
-                            lista = ProvaieVede.Domingo(mesExtenso);
-                            break;
-                        default:
-                            lista = ProvaieVede.Quarta(mesExtenso);
-                            break;
-                    }
-
-                    foreach (string result in lista)
-                    {
-                        if (result.Contains(dia))
-                        {                            
-                            caminhoArquivo = dir + result; //Endereço do arquivo. Ex.: arquivos\\provai_e_vede\\Sábado\\01_janeiro\\04_Uma Exceção.mp4
-                        }
-                    }
-
-                    if (File.Exists(caminhoArquivo))
-                    {
-                        //Abrir o arquivo
-                        Process.Start(caminhoArquivo);
-                        Thread.Sleep(2000);
-                        SendKeys.SendWait("%{ENTER}");
-                    }
-                    else
-                    {
-                        //Criando as pastas das semanas
-                        string path = @"arquivos\\provai_e_vede";
-                        Directory.CreateDirectory(path + "\\Domingo");
-                        Directory.CreateDirectory(path + "\\Quarta-Feira");
-                        Directory.CreateDirectory(path + "\\Sábado");
-
-                        Download download = new Download();
-                        download.abrir(caminhoArquivo);
-                    }
-                }
-                catch (Exception)
-                {
-                    string local = AppDomain.CurrentDomain.BaseDirectory.ToString() + "\\arquivos\\provai_e_vede";
-                    MessageBox.Show("Arquivo não encontrado, por favor, realize o download através do site\n https://downloads.adventistas.org/pt/projeto/provai-e-vede/ \nVocê também pode entrar em contato com a equipe da Coletânea de Louvor ou adquira a mídia com a sua Associação/Missão local.\n\nApós estar com o conteúdo em mãos, coloque os vídeos de acordo com o seu dia da semana e mês nesse local:\n" + local, "Falha ao encontrar o vídeo.", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }                   
+            switch (data.DayOfWeek) {
+                case DayOfWeek.Sunday:
+                    diaSemana = "Domingo";
+                    break;
+                case DayOfWeek.Saturday:
+                    diaSemana = "Sábado";
+                    break;
+                case DayOfWeek.Wednesday:
+                    diaSemana = "Quarta-Feira";
+                    break;
             }
-            else
+
+            if (diaSemana == "erro") {
                 MessageBox.Show("Escolha uma data válida e tente novamente.\n\nOBS.: Dias disponíveis: Domingo, Quarta-feira ou Sábado.", "Data inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string caminhoArquivo = string.Empty;
+            var dir = new DirectoryInfo(@"arquivos\\provai_e_vede\\" + diaSemana + "\\" + getMes(data.Month) + "\\");
+            if (!Directory.Exists(dir.FullName))
+                Directory.CreateDirectory(dir.FullName);
+
+            var files = Directory.GetFiles(dir.FullName, data.Day.ToString("00") + "*.mp4");
+            if(files.Length == 0) {
+                caminhoArquivo = dir.FullName + "\\" + data.Day.ToString("00") + "...mp4";
+                MessageBox.Show("Arquivo não encontrado: " + caminhoArquivo + "\nRealize o download no site \nhttps://downloads.adventistas.org/pt/projeto/provai-e-vede", "Falha ao encontrar o vídeo.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Process.Start(files[0]);
         }
     }
 }
