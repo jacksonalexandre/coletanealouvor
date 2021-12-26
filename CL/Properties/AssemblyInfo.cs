c974b4056384af1cc3e335cc9b2285018756bcd3
Jackson <jack@forlogic.org>
2021-12-26 19:11:36 -0300

    Ajustes

--- CL/Properties/AssemblyInfo.cs
+++ CL/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Adven.tech")]
 [assembly: AssemblyProduct("Coletânea de Louvor")]
-[assembly: AssemblyCopyright("Copyright © 2009-2019")]
+[assembly: AssemblyCopyright("Copyright © 2009-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("436b9ece-78ce-4c7c-bf32-04424a5bb535")]
 
-[assembly: AssemblyVersion("2019.02")]
-[assembly: AssemblyFileVersion("2019.02")]
+[assembly: AssemblyVersion("2021.01")]
+[assembly: AssemblyFileVersion("2021.01")]
